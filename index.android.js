7599a738cfe2f98e53e2fa7dbf3ace419154c06f
Matt Way <matt__way@hotmail.com>
2017-03-06 23:32:56 +1000

    initial setup

--- index.android.js
+++ index.android.js
@@ -10,102 +10,21 @@ import {
   StyleSheet,
   Text,
   View,
-  ViewPagerAndroid
+  WebView
 } from 'react-native';
 
-import YouTube from './YouTube';
-
-import Swiper from 'react-native-swiper';
-
-
-var dmmm = React.createClass({
-  render: function() {
-    return (
-      <ViewPagerAndroid
-      style={styles.container}
-      initialPage={1}>
-      
-        <View style={styles.other}>
-          <Text style={styles.instructions}>Beautiful</Text>
-        </View>
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit index.android.js
-          </Text>
-          <Text style={styles.instructions}>
-            Double tap R on your keyboard to reload,{'\n'}
-            Shake or press menu button for dev menu
-          </Text>
-          <YouTube
-            videoId="KVZ-P-ZI6W4"
-            play={true}
-            hidden={true}
-            playsInline={true}
-            showinfo={false}
-            controls={0}
-            apiKey={'AIzaSyDqc8oEwl5CbXC0-9zVkfJheLwMihACrG8'}          
-            style={{alignSelf: 'stretch', height: 250, backgroundColor: 'black', marginVertical: 10}}
-          />
-        </View>
-      </ViewPagerAndroid>
-    )
-  }
-});
-
-
-const styles = StyleSheet.create({
-  wrapper: {},
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FF0000'
-  },
-  other: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#0000FF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-/*
-class dmmm extends Component {
+export default class dmmm extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-        <YouTube
-          videoId="KVZ-P-ZI6W4"
-          play={true}
-          hidden={true}
-          playsInline={true}
-          showinfo={false}
-          controls={0}
-          apiKey={'AIzaSyDqc8oEwl5CbXC0-9zVkfJheLwMihACrG8'}          
-          style={{alignSelf: 'stretch', height: 250, backgroundColor: 'black', marginVertical: 10}}
+        <View style={{height: 200, width: '100%', backgroundColor: 'skyblue'}}>
+          <Text style={styles.welcome}>
+            Welcome to React Native
+          </Text>
+        </View>
+        <WebView
+          source={{uri: 'https://www.youtube.com/embed/PGUMRVowdv8'}}
+          style={styles.video}
         />
       </View>
     );
@@ -117,7 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FF0000'
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
@@ -129,6 +48,12 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  video: {
+    marginTop: 20,
+    maxHeight: 200,
+    width: 320,
+    flex: 1
+  }
 });
-*/
+
 AppRegistry.registerComponent('dmmm', () => dmmm);
--- index.ios.js
+++ index.ios.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-class dmmm extends Component {
+export default class dmmm extends Component {
   render() {
     return (
       <View style={styles.container}>
