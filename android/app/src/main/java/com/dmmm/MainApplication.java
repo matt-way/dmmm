42cb952e40b605aabe0476af5121414293f8e4d9
matt-way <matt@syntropy.xyz>
2019-06-05 15:49:25 +1000

    react-native-upgrade

--- android/app/src/main/java/com/dmmm/MainApplication.java
+++ android/app/src/main/java/com/dmmm/MainApplication.java
@@ -3,12 +3,6 @@ package com.dmmm;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import com.zmxv.RNSound.RNSoundPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.rnfs.RNFSPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,13 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SplashScreenReactPackage(),
-            new RNSoundPackage(),
-            new VectorIconsPackage(),
-            new LinearGradientPackage(),
-            new RNFetchBlobPackage(),
-            new RNFSPackage()
+          new MainReactPackage()
       );
     }
 
